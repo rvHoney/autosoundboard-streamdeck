9498c48ef331ae3c19dba02e918d541615c8aa3d
Robin <robin.vidal.pro@gmail.com>
2024-04-10 16:32:38 +0200

    Update

--- src/fr.pafadelcraft.autosoundboard.sdPlugin/app.js
+++ src/fr.pafadelcraft.autosoundboard.sdPlugin/app.js
@@ -7,7 +7,8 @@ const myAction = new Action('com.elgato.template.action');
  * The first event fired when Stream Deck starts
  */
 $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-	console.log('Stream Deck connected!');
+	const button = new StreamDeckButton(1, 1);
+	button.setTitle('Hello World!');
 });
 
 myAction.onKeyUp(({ action, context, device, event, payload }) => {
